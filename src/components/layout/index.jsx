22d9dd040e976fcc2b1df77749e1a799b5ccca5c
Vishnu21710 <vishnu.mupparathy21@gmail.com>
2024-07-22 13:52:29 +0530

    loading improved

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -5,29 +5,11 @@ import Footer from "../footer";
 import Auth from "../auth";
 import { Toaster } from "sonner";
 import ReferalModal from "../modals/referral-modal";
-import { API_URL } from "../../constants";
-import Loading from "../loading";
+
 
 const RootLayout = ({ children }) => {
 
-  const [backendLoading, setBackendLoading] = useState(true)
-
-  useEffect(()=>{
-    const checkBackend = async()=>{
-      try {
-        const response = await axios.get(`${API_URL}/health`)
-        console.log(response, "HEALTH CHECK");
-        setBackendLoading(false)
-      } catch (error) {
-       setBackendLoading(false) 
-      }
-    }
-    checkBackend()
-  }, [setBackendLoading])
-
-  if(backendLoading){
-    return <Loading/>
-  }
+  
 
   
   return (
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
-import Hero from '../../components/hero'
-import LogoTicker from '../../components/logo-ticker'
-import Features from '../../components/features'
-import Cta from '../../components/cta'
-import Faq from '../../components/faq'
+import React, { useEffect, useState } from "react";
+import Hero from "../../components/hero";
+import LogoTicker from "../../components/logo-ticker";
+import Features from "../../components/features";
+import Cta from "../../components/cta";
+import Faq from "../../components/faq";
+import axios from "axios";
+import { API_URL } from "../../constants";
+import Loading from "../../components/loading";
 
 const Home = () => {
-  return (
-    <main className='overflow-hidden'>
-        <Hero/>
-        <LogoTicker/>
-        <Features/>
-        <Faq/>
-        <Cta/>
-    </main>
-  )
-}
+  const [backendLoading, setBackendLoading] = useState(true);
 
-export default Home
+  useEffect(() => {
+    const checkBackend = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/health`);
+        console.log(response, "HEALTH CHECK");
+        setBackendLoading(false);
+      } catch (error) {
+        setBackendLoading(false);
+      }
+    };
+    checkBackend();
+  }, [setBackendLoading]);
+
+  if (backendLoading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <Loading />
+      </div>
+    );
+  }
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+      <LogoTicker />
+      <Features />
+      <Faq />
+      <Cta />
+    </main>
+  );
+};
+
+export default Home;
