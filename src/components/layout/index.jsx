640943c83b0603d557dc1d2d07cdc3ced426a366
Vishnu21710 <vishnu.mupparathy21@gmail.com>
2024-07-22 11:52:23 +0530

    loading fixed

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -5,8 +5,20 @@ import Footer from "../footer";
 import Auth from "../auth";
 import { Toaster } from "sonner";
 import ReferalModal from "../modals/referral-modal";
+import { useAuth } from "../../contexts/AuthContext";
+import Loading from "../loading";
 
 const RootLayout = ({ children }) => {
+
+  const auth = useAuth();
+
+  if (auth.loading) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  }
   return (
     <div>
       <Auth />
