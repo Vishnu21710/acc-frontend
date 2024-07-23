d4febecefdf0db2ac3e51ee2155a1686294aefbc
Vishnu21710 <vishnu.mupparathy21@gmail.com>
2024-07-23 08:46:37 +0530

    app loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,39 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
 import RootLayout from "./components/layout";
-import Signup from "./components/auth/signup";
-import Auth from "./components/auth";
 import AuthModalProvider from "./contexts/AuthModalContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import ReferalModalProvider from "./contexts/ReferralModalContext";
+import { useEffect, useState } from "react";
+import { API_URL } from "./constants";
+import Loading from "./components/loading";
 
 function App() {
+
+  const [backendLoading, setBackendLoading] = useState(true);
+
+  useEffect(() => {
+    const checkBackend = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/health`);
+        console.log(response, "HEALTH CHECK");
+        setBackendLoading(false);
+      } catch (error) {
+        setBackendLoading(false);
+      }
+    };
+    checkBackend();
+  }, [setBackendLoading]);
+
+  if (backendLoading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <Loading />
+      </div>
+    );
+  }
+
+
   return (
     <AuthModalProvider>
       <AuthProvider>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -9,28 +9,7 @@ import { API_URL } from "../../constants";
 import Loading from "../../components/loading";
 
 const Home = () => {
-  const [backendLoading, setBackendLoading] = useState(true);
-
-  useEffect(() => {
-    const checkBackend = async () => {
-      try {
-        const response = await axios.get(`${API_URL}/health`);
-        console.log(response, "HEALTH CHECK");
-        setBackendLoading(false);
-      } catch (error) {
-        setBackendLoading(false);
-      }
-    };
-    checkBackend();
-  }, [setBackendLoading]);
-
-  if (backendLoading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <Loading />
-      </div>
-    );
-  }
+ 
   return (
     <main className="overflow-hidden">
       <Hero />
